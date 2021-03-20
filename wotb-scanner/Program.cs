004e554fb378abaf03429f83a48bf804b4cd7a62
profesor79 <gitspam@profesor79.pl>
2021-03-20 11:05:24 +0000

    first code

---
+++ wotb-scanner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wotb_scanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
